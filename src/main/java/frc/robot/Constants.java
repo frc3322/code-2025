700cd3446602e23d006a374d70dbf7b748f16247
IBuildRoboats <grayakry@gmail.com>
2025-01-21 23:37:20 -0500

    swerve works, no more YAGSL

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,32 +1,37 @@
+// Copyright 2021-2025 FRC 6328
+// http://github.com/Mechanical-Advantage
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// version 3 as published by the Free Software Foundation or
+// available in the root directory of this project.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+
 package frc.robot;
 
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj.RobotBase;
 
-public class Constants {
-    public static final Mode currentMode = Mode.fromState();
-    
-    public static enum Mode {
-        /** Running on a real robot. */
-        REAL,
-    
-        /** Running a physics simulator. */
-        SIM,
-    
-        /** Replaying from a log file. */
-        REPLAY;
-    
-        static Mode fromState() {
-          if (Robot.isReal()) {
-            return REAL;
-          } else {
-            return SIM;
-          }
-        }
-      }
+/**
+ * This class defines the runtime mode used by AdvantageKit. The mode is always "real" when running
+ * on a roboRIO. Change the value of "simMode" to switch between "sim" (physics sim) and "replay"
+ * (log replay from a file).
+ */
+public final class Constants {
+  public static final Mode simMode = Mode.SIM;
+  public static final Mode currentMode = RobotBase.isReal() ? Mode.REAL : simMode;
 
-    public static final class OIConstants {
-        public static final CommandXboxController driverController = new CommandXboxController(0);
-        public static final CommandXboxController operatorController = new CommandXboxController(1);
-    }
+  public static enum Mode {
+    /** Running on a real robot. */
+    REAL,
 
+    /** Running a physics simulator. */
+    SIM,
+
+    /** Replaying from a log file. */
+    REPLAY
+  }
 }
