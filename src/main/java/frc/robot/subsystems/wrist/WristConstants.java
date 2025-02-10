3457777c46401dfc4d215bf716ec2464f8bd022d
DarkEden-coding <darkedenc9@gmail.com>
2025-02-10 15:30:23 -0500

    Add Wrist Sim

--- src/main/java/frc/robot/subsystems/wrist/WristConstants.java
+++ src/main/java/frc/robot/subsystems/wrist/WristConstants.java
@@ -3,11 +3,29 @@ package frc.robot.subsystems.wrist;
 public class WristConstants {
   public static final int wristMotorCurrentLimit = 40;
 
+  public static double rotationsToRadians(double rotations) {
+    return rotations * 2 * Math.PI;
+  }
+
+  public static double radiansToRotations(double radians) {
+    return radians / (2 * Math.PI);
+  }
+
   public static class GearboxConstants {
     public static final double gearRatio = .01;
   }
 
   public static class ControllerConstants {
+    public static final double kP = 0;
+    public static final double kI = 0;
+    public static final double kD = 0;
+    public static final double velocityConstraint = 1;
+    public static final double accelerationConstraint = 2;
+    public static final double positionTolerance = 0;
+    public static final double velocityTolerance = 0;
+  }
+
+  public static final class SimConstants {
     public static final double kP = 0;
     public static final double kI = 0;
     public static final double kD = 0;
--- src/main/java/frc/robot/subsystems/wrist/WristIOSim.java
+++ src/main/java/frc/robot/subsystems/wrist/WristIOSim.java
@@ -1,3 +1,53 @@
 package frc.robot.subsystems.wrist;
 
-public class WristIOSim implements WristIO {}
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+import frc.robot.subsystems.wrist.WristConstants.GearboxConstants;
+import frc.robot.subsystems.wrist.WristConstants.SimConstants;
+
+public class WristIOSim implements WristIO {
+  private DCMotor wristMotor = DCMotor.getNEO(1);
+
+  private DCMotorSim wristMotorSim = new DCMotorSim(LinearSystemId.createDCMotorSystem(wristMotor, 1, GearboxConstants.gearRatio), wristMotor);
+
+  private ProfiledPIDController wristPID =
+      new ProfiledPIDController(
+          SimConstants.kP,
+          SimConstants.kI,
+          SimConstants.kD,
+          new Constraints(SimConstants.velocityConstraint, SimConstants.accelerationConstraint));
+
+  private double pidOuput = 0;
+
+  public WristIOSim() {
+    wristPID.setTolerance(SimConstants.positionTolerance, SimConstants.velocityTolerance);
+  }
+
+  @Override
+  public void goToPosition(double targetPositionRotations) {
+    double targetPositionRadians = WristConstants.rotationsToRadians(targetPositionRotations);
+
+    wristPID.setGoal(targetPositionRadians);
+
+    pidOuput = wristPID.calculate(wristMotorSim.getAngularPositionRad());
+
+    wristMotorSim.setInputVoltage(pidOuput * 12);
+  }
+
+  public void updateInputs(WristIOInputsAutoLogged inputs) {
+    wristMotorSim.update(0.020);
+
+    inputs.absolutePosition = WristConstants.radiansToRotations(wristMotorSim.getAngularPositionRad());
+    inputs.absoluteVelocity = WristConstants.radiansToRotations(wristMotorSim.getAngularVelocityRadPerSec());
+
+    inputs.motorPower = pidOuput;
+
+    inputs.setpoint = WristConstants.radiansToRotations(wristPID.getGoal().position);
+    inputs.pidOut = pidOuput;
+
+    inputs.atGoal = wristPID.atGoal();
+  }
+}
