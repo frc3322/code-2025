e5d7ced8d59039938a2b5a3616c318202e2f3f1b
IBuildRoboats <grayakry@gmail.com>
2025-02-17 02:27:32 -0500

    added teleopinit command

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -136,6 +136,7 @@ public class Robot extends LoggedRobot {
     if (autonomousCommand != null) {
       autonomousCommand.cancel();
     }
+    robotContainer.onTeleopInitCommand().schedule();
   }
 
   /** This function is called periodically during operator control. */
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,6 +19,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.Constants.FieldConstants.ReefConstants;
@@ -299,6 +300,12 @@ public class RobotContainer {
     //                 drive)
     //             .ignoringDisable(true));
 
+  }
+
+  public Command onTeleopInitCommand() {
+    return new ParallelCommandGroup(
+        pivot.releaseManualDirectionCommand(),
+        superstructure.setSuperStateCommand(SuperState.STOW));
   }
 
   /**
