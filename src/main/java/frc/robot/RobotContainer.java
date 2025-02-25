a53d09fc692369956be681c2e8a001d5094c8e77
IBuildRoboats <grayakry@gmail.com>
2025-02-25 09:34:10 -0500

    undo untested speed changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -165,17 +165,7 @@ public class RobotContainer {
     // Set up named commands
     NamedCommands.registerCommand("STOW", superstructure.setSuperStateCommand(SuperState.STOW));
     NamedCommands.registerCommand(
-        "L4 SCORE", superstructure.l4ScoreCommand().andThen(new WaitCommand(.1)));
-
-    NamedCommands.registerCommand(
-        "AUTO ALIGN L1",
-        new ParallelCommandGroup(
-            superstructure.setTargetLevelCommand(SuperState.REEFL1),
-            simpledrive
-                .autoDrive(superstructure::getTargetReefPose)
-                .withTimeout(.35)
-                .andThen(DriveCommands.stopCommand(drive)),
-            superstructure.setSuperStateCommand(SuperState.REEFL1)));
+        "L4 SCORE", superstructure.l4ScoreCommand().andThen(new WaitCommand(.25)));
 
     NamedCommands.registerCommand(
         "L1 SCORE", superstructure.l1ScoreCommand().andThen(new WaitCommand(.25)));
@@ -204,7 +194,7 @@ public class RobotContainer {
         "AUTO ALIGN",
         simpledrive
             .autoDrive(superstructure::getTargetReefPose)
-            .withTimeout(.3)
+            .withTimeout(.65)
             .andThen(DriveCommands.stopCommand(drive)));
 
     NamedCommands.registerCommand(
